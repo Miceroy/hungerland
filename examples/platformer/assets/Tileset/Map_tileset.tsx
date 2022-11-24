a90d3238e3457ad65f22d1dbcec3b0a3d67f3584
Mikko Romppainen <mikko.romppainen@kamk.fi>
2022-11-24 11:04:13 +0200

    Added some platformer assets.

---
+++ examples/platformer/assets/Tileset/Map_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Map_tileset" tilewidth="64" tileheight="64" tilecount="300" columns="30">
+ <image source="Tileset.png" width="1920" height="640"/>
+</tileset>
