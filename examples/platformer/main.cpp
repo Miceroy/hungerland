3fc0e42f21c881e66acfff74dd919bdeb928b648
Mikko Romppainen <mikko.romppainen@kamk.fi>
2022-11-25 10:39:36 +0200

    Update.

--- examples/platformer/main.cpp
+++ examples/platformer/main.cpp
@@ -1,13 +1,24 @@
-#include "./platformer.h"
+#include "../platformer.h"
 
-namespace example {
-	/// CONFIG:
-/*	static const size_t	WINDOW_SIZE_X = 1920;
-	static const size_t	WINDOW_SIZE_Y = 1080;
-	static const float	SCALE_X = 48.0f;
-	static const float	SCALE_Y = 48.0f;*/
-	static const std::string GAME_NAME = "Platformer Game";
-	static const std::string GAME_VERSION = "v0.0.1";
+namespace cs {
+	// Resource files:
+	static const std::vector<std::string> MAP_FILES = {
+        "assets/untitled.tmx",
+	};
+
+    static const std::vector<std::string> BACKGROUND_FILES = {
+	};
+
+    static const std::vector<std::string> CHARACTER_TEXTURE_FILES = {
+        "assets/Player/Player_NoAnimation.png",
+	};
+
+	static const std::vector<std::string> ITEM_TEXTURE_FILES = {
+	};
+
+	// Application config:
+    static const std::string GAME_NAME = "Platformer Example";
+    static const std::string GAME_VERSION = "v0.0.1";
 	static const std::string GAME_LONG_NAME = GAME_NAME + " " + GAME_VERSION;
 }
 
@@ -16,15 +27,21 @@ int main() {
 	using namespace mikroplot;
 	using namespace platformer;
 	using namespace app;
-	using namespace example;
+	using namespace cs;
 	// Create application window and run it.
 	mikroplot::Window window(WINDOW_SIZE_X/2, WINDOW_SIZE_Y/2, "");
 	Functor f;
 	typedef World<GameObject> World;
-	f.loadTexture = [&window](const std::string& fileName) {
-		return window.loadTexture(fileName);
+    f.loadTexture = [&window](const std::string& fileName, bool repeat) {
+        return window.loadTexture(fileName, repeat);
 	};
-	auto state = reset<Functor,World>(f, GAME_LONG_NAME, "world1");
+	static const platformer::Config CONFIG = {
+		BACKGROUND_FILES,
+		MAP_FILES,
+		CHARACTER_TEXTURE_FILES,
+		ITEM_TEXTURE_FILES
+	};
+	auto state = reset<Functor,World>(f, GAME_LONG_NAME, CONFIG);
 	return 	window.run([&](mikroplot::Window& window, float dt, float time) {
 		app::render(window, update(window, state, f, dt), time);
 	});
